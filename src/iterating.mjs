9fe9001798b0808f64344972ca3030ded85eda10
Visa Hannula <visa@viha.fi>
2020-08-22 22:49:50 +0300

    iterating exercises and fix import issue

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -1,16 +1,51 @@
 import setText , {appendText} from './results.mjs';
+//import { axios } from './axios.min.js';
 
-export function get(){
+export async function get(){
+    let {data} = await axios.get('/orders/1');
+    setText(JSON.stringify(data));
 }
 
-export function getCatch(){
+export async function getCatch(){
+    try {
+        let {data} = await axios.get('/orders/123');
+        setText(JSON.stringify(data));
+    } catch (error) {
+        setText(`Cannot get order. ${error}`);
+    }
 }
 
-export function chain(){
+export async function chain(){
+    let {data} = await axios.get('/orders/1');
+    let {data: address} = await axios.get(`/addresses/${data.shippingAddress}`);
+    setText(JSON.stringify(address));
 }
 
-export function concurrent(){
+export async function concurrent(){
+    const orderStatus = axios.get('/orderStatuses');
+    const orders = axios.get('/orders');
+
+    setText("");
+
+    const {data: statuses} = await orderStatus;
+    const {data: order} = await orders;
+
+    appendText(`Statuses: ${JSON.stringify(statuses)}\n`);
+    appendText(`Order 1: ${JSON.stringify(order[0])}\n`);
+
 }
 
-export function parallel(){
+export async function parallel(){
+    setText("");
+
+    await Promise.all([
+        (async () => {
+            const {data} = await axios.get('/orderStatuses');
+            appendText(JSON.stringify(data));
+        })(),
+        (async () => {
+            const {data} = await axios.get('/orders');
+            appendText(JSON.stringify(data[0]));
+        })()
+    ])
 }
